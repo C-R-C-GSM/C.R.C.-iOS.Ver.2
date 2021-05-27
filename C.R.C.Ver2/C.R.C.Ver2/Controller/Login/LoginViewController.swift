211b98c80edf055cac62a167a19f6f4986a6a31a
jo030225 <whwngur123@naver.com>
2021-05-27 11:56:47 +0900

    [CREATE] LoginViewController 생성

---
+++ C.R.C.Ver2/C.R.C.Ver2/Controller/Login/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  C.R.C.Ver2
+//
+//  Created by 조주혁 on 2021/05/27.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
