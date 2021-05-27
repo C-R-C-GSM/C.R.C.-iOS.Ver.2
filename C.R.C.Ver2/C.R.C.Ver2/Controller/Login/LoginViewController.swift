0c48da92fc906bd6319246de87eaf24d940e1136
jo030225 <whwngur123@naver.com>
2021-05-27 20:40:16 +0900

    [ADD] UI 수정 및 IBOutlet 변수 연결

--- C.R.C.Ver2/C.R.C.Ver2/Controller/Login/LoginViewController.swift
+++ C.R.C.Ver2/C.R.C.Ver2/Controller/Login/LoginViewController.swift
@@ -9,12 +9,27 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setting()
+    }
 
+    func setting() {
         self.navigationController?.navigationBar.shadowImage = UIImage()
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         self.navigationController?.navigationBar.clipsToBounds = true
+        
+        signInBtn.layer.cornerRadius = 10
     }
-
+    
+    @IBAction func signInButton(_ sender: UIButton) {
+        
+    }
+    
+    
 }
