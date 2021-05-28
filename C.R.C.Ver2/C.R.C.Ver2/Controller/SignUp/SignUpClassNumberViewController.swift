fdfe651709afcf4c5ed9dee9a06fcf603c20c21c
jo030225 <whwngur123@naver.com>
2021-05-28 16:17:43 +0900

    [ADD] 오토레이아웃 수정 및 키보드 올리기 구현

--- C.R.C.Ver2/C.R.C.Ver2/Controller/SignUp/SignUpClassNumberViewController.swift
+++ C.R.C.Ver2/C.R.C.Ver2/Controller/SignUp/SignUpClassNumberViewController.swift
@@ -11,6 +11,7 @@ class SignUpClassNumberViewController: UIViewController {
 
     @IBOutlet weak var classNumberTextField: UITextField!
     @IBOutlet weak var doneBtn: UIButton!
+    @IBOutlet weak var doneBtnBottomConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,12 +19,62 @@ class SignUpClassNumberViewController: UIViewController {
         setting()
         
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver( self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil )
+        NotificationCenter.default.addObserver( self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil )
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
     @IBAction func doneButton(_ sender: UIButton) {
         navigationController?.popToRootViewController(animated: true)
     }
     
     func setting() {
+        classNumberTextField.delegate = self
         doneBtn.layer.cornerRadius = 10
     }
 
+}
+
+extension SignUpClassNumberViewController: UITextFieldDelegate {
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keybaordRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keybaordRectangle.height
+            
+            if self.doneBtnBottomConstraint.constant == 30 {
+                self.doneBtnBottomConstraint.constant += keyboardHeight - 40
+                self.view.layoutIfNeeded()
+            }
+            
+        }
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        if self.doneBtnBottomConstraint.constant != 30 {
+            self.doneBtnBottomConstraint.constant = 30
+            self.view.layoutIfNeeded()
+        }
+        
+    }
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        classNumberTextField.resignFirstResponder()
+        return true
+    }
 }
--- C.R.C.Ver2/C.R.C.Ver2/Controller/SignUp/SignUpNameViewController.swift
+++ C.R.C.Ver2/C.R.C.Ver2/Controller/SignUp/SignUpNameViewController.swift
@@ -11,6 +11,7 @@ class SignUpNameViewController: UIViewController {
 
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var continueBtn: UIButton!
+    @IBOutlet weak var continueBtnBottomConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,10 +19,61 @@ class SignUpNameViewController: UIViewController {
         setting()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver( self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil )
+        NotificationCenter.default.addObserver( self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil )
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
 
     func setting() {
         self.navigationItem.backBarButtonItem?.tintColor = .init(named: "Primary Color")
+        
+        nameTextField.delegate = self
+        
         continueBtn.layer.cornerRadius = 10
     }
 
+}
+
+extension SignUpNameViewController: UITextFieldDelegate {
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keybaordRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keybaordRectangle.height
+            
+            if self.continueBtnBottomConstraint.constant == 30 {
+                self.continueBtnBottomConstraint.constant += keyboardHeight - 40
+                self.view.layoutIfNeeded()
+            }
+            
+        }
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        if self.continueBtnBottomConstraint.constant != 30 {
+            self.continueBtnBottomConstraint.constant = 30
+            self.view.layoutIfNeeded()
+        }
+        
+    }
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        nameTextField.resignFirstResponder()
+        return true
+    }
 }
