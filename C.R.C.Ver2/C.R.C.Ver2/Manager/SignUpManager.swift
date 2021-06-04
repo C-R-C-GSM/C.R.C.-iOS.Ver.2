80f746b6406fd21008973531ffec96eeededa946
jo030225 <whwngur123@naver.com>
2021-06-04 11:18:02 +0900

    [ADD] SignUpManager 구현

--- C.R.C.Ver2/C.R.C.Ver2/Manager/SignUpManager.swift
+++ C.R.C.Ver2/C.R.C.Ver2/Manager/SignUpManager.swift
@@ -9,5 +9,57 @@ import Foundation
 import KeychainAccess
 
 class SignUpManager {
+    static let keychain = Keychain(service: "com.yourcompany.C-R-C-Ver2")
     
+    // email
+    class func saveEmail(email: String) {
+        keychain["email"] = email
+    }
+    
+    class func getEmail() -> String {
+        if let email = keychain["email"] {
+            return email
+        } else {
+            return ""
+        }
+    }
+    
+    class func removeEmail() {
+        keychain["email"] = nil
+    }
+    
+    
+    // password
+    class func savePassword(password: String) {
+        keychain["password"] = password
+    }
+    
+    class func getPassword() -> String {
+        if let password = keychain["password"] {
+            return password
+        } else {
+            return ""
+        }
+    }
+    
+    class func removePassword() {
+        keychain["password"] = nil
+    }
+    
+    // name
+    class func saveName(name: String) {
+        keychain["name"] = name
+    }
+    
+    class func getName() -> String {
+        if let name = keychain["name"] {
+            return name
+        } else {
+            return ""
+        }
+    }
+    
+    class func removeName() {
+        keychain["name"] = nil
+    }
 }
