49a4dce9c55fc6f29d1140f9555f121c1a12e184
jo030225 <whwngur123@naver.com>
2021-06-05 22:32:00 +0900

    [CREATE] StudentStatusBoardTableViewCell 파일 생성

---
+++ C.R.C.Ver2/C.R.C.Ver2/View/Cell/StudentStatusBoard/StudentStatusBoardTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  StudentStatusBoardTableViewCell.swift
+//  C.R.C.Ver2
+//
+//  Created by 조주혁 on 2021/06/05.
+//
+
+import UIKit
+
+class StudentStatusBoardTableViewCell: UITableViewCell {
+
+    
+
+}
