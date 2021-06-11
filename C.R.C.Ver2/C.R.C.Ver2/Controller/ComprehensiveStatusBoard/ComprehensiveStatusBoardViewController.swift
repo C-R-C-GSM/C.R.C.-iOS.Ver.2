22137e2c587b98e29a5fdf2c48fe7fe79ad42da7
jo030225 <whwngur123@naver.com>
2021-06-11 16:13:52 +0900

    [CREATE] ComprehensiveStatusBoardViewController 파일 생성

---
+++ C.R.C.Ver2/C.R.C.Ver2/Controller/ComprehensiveStatusBoard/ComprehensiveStatusBoardViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ComprehensiveStatusBoardViewController.swift
+//  C.R.C.Ver2
+//
+//  Created by 조주혁 on 2021/06/11.
+//
+
+import UIKit
+
+class ComprehensiveStatusBoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
