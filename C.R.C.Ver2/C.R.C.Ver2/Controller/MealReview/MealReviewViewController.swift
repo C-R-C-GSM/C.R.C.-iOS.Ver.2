8543c827311ce682b1355ad09599ffd69102013b
jo030225 <whwngur123@naver.com>
2021-06-13 19:23:42 +0900

    [CREATE] MealReviewViewController 생성 및 연결

---
+++ C.R.C.Ver2/C.R.C.Ver2/Controller/MealReview/MealReviewViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MealReviewViewController.swift
+//  C.R.C.Ver2
+//
+//  Created by 조주혁 on 2021/06/13.
+//
+
+import UIKit
+
+class MealReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
