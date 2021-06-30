f965ed0941569fac312d6a696b90dce1138ef1e4
jo030225 <whwngur123@naver.com>
2021-06-30 18:02:37 +0900

    [ADD] 종합 현황판 구현

--- C.R.C.Ver2/C.R.C.Ver2/Controller/TotalStatusBoard/TotalStatusBoardViewController.swift
+++ C.R.C.Ver2/C.R.C.Ver2/Controller/TotalStatusBoard/TotalStatusBoardViewController.swift
@@ -6,20 +6,44 @@
 //
 
 import UIKit
+import Alamofire
 
 class TotalStatusBoardViewController: UIViewController {
 
-    @IBOutlet weak var totalStudentCount: UILabel!
+    var model: TotalStudentModel?
+    
     @IBOutlet weak var comeStudentCount: UILabel!
     @IBOutlet weak var notComeStudentCount: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
+        apiCall()
     }
+    
+    func setting() {
+        comeStudentCount.text = "\(model?.total_num ?? 0)"
+        notComeStudentCount.text = "\(230 - (model?.total_num ?? 0))"
+    }
+    
+    func apiCall() {
+        let URL = "http://10.120.75.224:3000/check/total"
+        let token = TokenManager.getToken()
+        AF.request(URL, method: .get, headers: ["Token": token]).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                guard let data = response.data else { return }
+                self.model = try? JSONDecoder().decode(TotalStudentModel.self, from: data)
+                self.setting()
+                print(value)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
     @IBAction func refreshButton(_ sender: UIBarButtonItem) {
-        
+        apiCall()
     }
     
 }
