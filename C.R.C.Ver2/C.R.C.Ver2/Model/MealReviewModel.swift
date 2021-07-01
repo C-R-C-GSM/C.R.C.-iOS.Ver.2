6d5b2f9077f39a433b8675d0dac15b4b4d5fc5fb
jo030225 <whwngur123@naver.com>
2021-07-01 21:15:30 +0900

    [CREATE] MealReviewModel 생성

---
+++ C.R.C.Ver2/C.R.C.Ver2/Model/MealReviewModel.swift
@@ -0,0 +1,24 @@
+//
+//  MealReviewModel.swift
+//  C.R.C.Ver2
+//
+//  Created by 조주혁 on 2021/07/01.
+//
+
+import Foundation
+
+struct MealReviewModel: Codable {
+    let code: Int
+    let review_data: [Review_Data]
+}
+
+struct Review_Data: Codable {
+    let reviewid: Int
+    let nickname: String
+    let review_star: Int
+    let content: String
+    let empathy: Int
+    let review_time: String
+    let review_when: String
+    let reply: String
+}
