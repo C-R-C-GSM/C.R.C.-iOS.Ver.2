7ec85726d93c1ebca853099b5acddce36e33c3f2
jo030225 <whwngur123@naver.com>
2021-07-03 19:10:02 +0900

    [ADD] UI 구현

--- C.R.C.Ver2/C.R.C.Ver2/Controller/MealSuggestionBoard/MealSuggestionBoardViewController.swift
+++ C.R.C.Ver2/C.R.C.Ver2/Controller/MealSuggestionBoard/MealSuggestionBoardViewController.swift
@@ -9,9 +9,30 @@ import UIKit
 
 class MealSuggestionBoardViewController: UIViewController {
 
+    @IBOutlet weak var mealSuggestionBoardTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        setting()
     }
+    
+    func setting() {
+        mealSuggestionBoardTableView.delegate = self
+        mealSuggestionBoardTableView.dataSource = self
+        mealSuggestionBoardTableView.tableFooterView = UIView()
+    }
+}
+
+extension MealSuggestionBoardViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MealSuggestionBoardTableViewCell", for: indexPath) as! MealSuggestionBoardTableViewCell
+        return cell
+    }
+    
+    
 }
