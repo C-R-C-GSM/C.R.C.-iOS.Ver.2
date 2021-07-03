72ba5a1dc703d329a7a64293bb4f41ecb55af51b
jo030225 <whwngur123@naver.com>
2021-07-03 19:00:31 +0900

    [CREATE] MealSuggestionBoardTableViewCell 생성

---
+++ C.R.C.Ver2/C.R.C.Ver2/View/Cell/MealSuggestionBoard/MealSuggestionBoardTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MealSuggestionBoardTableViewCell.swift
+//  C.R.C.Ver2
+//
+//  Created by 조주혁 on 2021/07/03.
+//
+
+import UIKit
+
+class MealSuggestionBoardTableViewCell: UITableViewCell {
+
+   
+
+}
