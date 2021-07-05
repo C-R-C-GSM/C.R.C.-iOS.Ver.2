ef66eb52b9e2ec415fca3167cb7cbe4f25c32e83
jo030225 <whwngur123@naver.com>
2021-07-05 12:21:31 +0900

    [CREATE] MealSuggestionBoardModel 생성

---
+++ C.R.C.Ver2/C.R.C.Ver2/Model/MealSuggestionBoardModel.swift
@@ -0,0 +1,21 @@
+//
+//  MealSuggestionBoardModel.swift
+//  C.R.C.Ver2
+//
+//  Created by 조주혁 on 2021/07/05.
+//
+
+import Foundation
+
+struct MealSuggestionBoardModel: Codable {
+    let code: Int
+    let suggest_data: [Suggest_Data]
+}
+
+struct Suggest_Data: Codable {
+    let suggestid: Int
+    let title: String
+    let content: String
+    let suggest_time: String
+    let nickname: String
+}
