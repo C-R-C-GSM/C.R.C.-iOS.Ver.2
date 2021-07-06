ba28daafe6b8e2c1937763c224d3df145e4be399
jo030225 <whwngur123@naver.com>
2021-07-06 15:19:21 +0900

    [CREATE] MealSuggestionBoardCreateViewController 생성

---
+++ C.R.C.Ver2/C.R.C.Ver2/Controller/MealSuggestionBoard/MealSuggestionBoardCreateViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MealSuggestionBoardCreateViewController.swift
+//  C.R.C.Ver2
+//
+//  Created by 조주혁 on 2021/07/06.
+//
+
+import UIKit
+import Alamofire
+
+class MealSuggestionBoardCreateViewController: UIViewController {
+
+    @IBOutlet weak var mealSuggestionNickname: UITextField!
+    @IBOutlet weak var mealSuggestionTitle: UITextField!
+    @IBOutlet weak var mealSuggestionContent: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
