2d57cfe308689e48c917f62ea4d031aa489fef96
jo030225 <whwngur123@naver.com>
2021-07-07 17:41:47 +0900

    [CREATE] AddViewController, NoticeViewController 생성

---
+++ C.R.C.Ver2/C.R.C.Ver2/Controller/ADD/AddViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddViewController.swift
+//  C.R.C.Ver2
+//
+//  Created by 조주혁 on 2021/07/07.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
---
+++ C.R.C.Ver2/C.R.C.Ver2/Controller/ADD/NoticeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NoticeViewController.swift
+//  C.R.C.Ver2
+//
+//  Created by 조주혁 on 2021/07/07.
+//
+
+import UIKit
+
+class NoticeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
