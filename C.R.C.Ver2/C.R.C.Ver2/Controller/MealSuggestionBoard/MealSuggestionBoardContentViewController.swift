d52f93c65b28b403bf0da1d0b1aa0817d5d368dc
jo030225 <whwngur123@naver.com>
2021-07-11 17:55:46 +0900

    [ADD] 급식 건의 답글 달기 구현

--- C.R.C.Ver2/C.R.C.Ver2/Controller/MealSuggestionBoard/MealSuggestionBoardContentViewController.swift
+++ C.R.C.Ver2/C.R.C.Ver2/Controller/MealSuggestionBoard/MealSuggestionBoardContentViewController.swift
@@ -6,12 +6,52 @@
 //
 
 import UIKit
+import Alamofire
 
 class MealSuggestionBoardContentViewController: UIViewController {
 
+    @IBOutlet weak var mealSuggestionTitle: UILabel!
+    @IBOutlet weak var mealSuggestionDate: UILabel!
+    @IBOutlet weak var mealSuggestionNickname: UILabel!
+    @IBOutlet weak var mealSuggestionContent: UILabel!
+    @IBOutlet weak var mealSuggestionAnswer: UILabel!
+    
+    @IBOutlet weak var mealSuggestionAnswerButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        setting()
+        checkApiCall()
+    }
+    
+    func setting() {
+        mealSuggestionTitle.text = MealSuggestionBoardManager.getMealSuggestionTitle()
+        mealSuggestionDate.text = MealSuggestionBoardManager.getMealSuggestionDate()
+        mealSuggestionNickname.text = MealSuggestionBoardManager.getMealSuggestionNickname()
+        mealSuggestionContent.text = MealSuggestionBoardManager.getMealSuggestionContent()
+        mealSuggestionAnswer.text = MealSuggestionBoardManager.getMealSuggestionAnswer()
+    }
+    
+    func checkApiCall() {
+        let URL = "http://ec2-52-14-165-111.us-east-2.compute.amazonaws.com:3000/check/role"
+        let token = TokenManager.getToken()
+        AF.request(URL, method: .get, headers: ["Token": token]).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                if let dic = value as? NSDictionary, let code = dic["code"] as? Int {
+                    switch code {
+                    case -600:
+                        break
+                    default:
+                        self.mealSuggestionAnswerButton.isEnabled = true
+                        self.mealSuggestionAnswerButton.tintColor = .init(named: "Primary Color")
+                    }
+                }
+                print(value)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 }
