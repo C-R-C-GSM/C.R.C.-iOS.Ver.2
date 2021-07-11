1e8ff626c846576ddb968fd743bd12412d48af57
jo030225 <whwngur123@naver.com>
2021-07-11 16:56:37 +0900

    [CREATE] MealSuggestionBoardManager 생성

---
+++ C.R.C.Ver2/C.R.C.Ver2/Manager/MealSuggestionBoardManager.swift
@@ -0,0 +1,73 @@
+//
+//  MealSuggestionBoardManager.swift
+//  C.R.C.Ver2
+//
+//  Created by 조주혁 on 2021/07/11.
+//
+
+import Foundation
+
+class MealSuggestionBoardManager {
+    static var mealSuggestionId: Int?
+    static var mealSuggestionTitle: String?
+    static var mealSuggestionContent: String?
+    static var mealSuggestionDate: String?
+    
+    // 급식 건의 Id
+    class func getMealSuggestionId() -> Int {
+        guard let id = mealSuggestionId else { return 0 }
+        return id
+    }
+    
+    class func saveMealSuggestionId(id: Int) {
+        mealSuggestionId = id
+    }
+    
+    class func removeMealSuggestionId() {
+        mealSuggestionId = nil
+    }
+    
+    
+    // 급식 건의 제목
+    class func getMealSuggestionTitle() -> String {
+        guard let title = mealSuggestionTitle else { return "" }
+        return title
+    }
+    
+    class func saveMealSuggestionTitle(title: String) {
+        mealSuggestionTitle = title
+    }
+    
+    class func removeMealSuggestionTitle() {
+        mealSuggestionTitle = nil
+    }
+    
+    
+    // 급식 건의 내용
+    class func getMealSuggestionContent() -> String {
+        guard let content = mealSuggestionContent else { return "" }
+        return content
+    }
+    
+    class func saveMealSuggestionContent(content: String) {
+        mealSuggestionContent = content
+    }
+    
+    class func removeMealSuggestionContent() {
+        mealSuggestionContent = nil
+    }
+    
+    // 급식 건의 작성 날짜
+    class func getMealSuggestionDate() -> String {
+        guard let date = mealSuggestionDate else { return "" }
+        return date
+    }
+    
+    class func saveMealSuggestionDate(date: String) {
+        mealSuggestionDate = date
+    }
+    
+    class func removeMealSuggestionDate() {
+        mealSuggestionDate = nil
+    }
+}
