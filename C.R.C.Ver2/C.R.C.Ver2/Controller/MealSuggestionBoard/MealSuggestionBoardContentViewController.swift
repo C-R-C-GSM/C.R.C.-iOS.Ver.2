18acc7892883afa5e9512a1bb020dba397d3d509
jo030225 <whwngur123@naver.com>
2021-07-11 16:18:14 +0900

    [CREATE] MealSuggestionBoardContentViewController 생성

---
+++ C.R.C.Ver2/C.R.C.Ver2/Controller/MealSuggestionBoard/MealSuggestionBoardContentViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MealSuggestionBoardContentViewController.swift
+//  C.R.C.Ver2
+//
+//  Created by 조주혁 on 2021/07/11.
+//
+
+import UIKit
+
+class MealSuggestionBoardContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
