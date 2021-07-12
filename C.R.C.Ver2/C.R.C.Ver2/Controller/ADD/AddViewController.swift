9e3dc0380c75191e07ed2ef31c9e97263c1c6a0a
jo030225 <whwngur123@naver.com>
2021-07-12 11:16:37 +0900

    [ADD] 로그아웃 기능 구현

--- C.R.C.Ver2/C.R.C.Ver2/Controller/ADD/AddViewController.swift
+++ C.R.C.Ver2/C.R.C.Ver2/Controller/ADD/AddViewController.swift
@@ -12,6 +12,27 @@ class AddViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    @IBAction func logoutButton(_ sender: UIButton) {
+        logOutAlert()
+    }
+    
+    func logOutAlert() {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let ok = UIAlertAction(title: "로그아웃", style: .destructive) { (_) in
+            self.goLoginPage()
+        }
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
         
+    }
+    
+    func goLoginPage(){
+        guard let goMain = self.storyboard?.instantiateViewController(identifier: "LoginViewController") else { return }
+        goMain.modalPresentationStyle = .fullScreen
+        self.present(goMain, animated: true)
     }
 }
